bacef60ee83e6c37e7c992795faa5529132cb98e
wilder <anthonykwanchung@gmail.com>
2019-11-20 18:05:28 +0100

    add show page

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -17,4 +17,23 @@ class WildController extends AbstractController
             'website' => 'Wild Séries',
         ]);
     }
+
+    /**
+    * @Route("/wild/show/{slug}",
+    *     requirements={"slug"="[a-z0-9\-]+"},
+     *    methods={"GET"},
+     *     defaults={"slug"="Aucune série sélectionnée, veuillez choisir une série"},
+    *     name="wild_show"
+        * )
+    */
+    public function show($slug) :Response
+    {
+        $slug = explode("-", $slug);
+        $sentence = implode(" ", $slug);
+        $slug = ucwords($sentence);
+
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug
+        ]);
+    }
 }
