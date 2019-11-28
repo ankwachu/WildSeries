9331d64bdcdd7f93f5b358afeb0a030a989e5e33
wilder <anthonykwanchung@gmail.com>
2019-11-28 17:01:49 +0100

    quete11

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramSearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -29,8 +31,19 @@ class WildController extends AbstractController
             );
         }
 
+        $form = $this->createForm(
+
+            ProgramSearchType::class,
+
+            null,
+
+            ['method' => Request::METHOD_GET]
+
+        );
+
         return $this->render('wild/index.html.twig', [
-            'programs' => $programs
+            'programs' => $programs,
+            'form' => $form->createView(),
         ]);
     }
 
