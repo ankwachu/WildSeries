68a6da65faf1e1a2e04b47c08f318430cb807766
wilder <anthonykwanchung@gmail.com>
2019-12-01 13:32:46 +0100

    quete 14

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,6 +19,14 @@ class ProgramType extends AbstractType
             ->add('poster')
             ->add('category', null, ['choice_label' => 'name'])
         ;
+        $builder->add('actors', EntityType::class, [
+            'class' => Actor::class,
+            'choice_label' => 'name',
+            'multiple' => true,
+            'expanded' => true,
+            'by_reference' => false,
+        ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
