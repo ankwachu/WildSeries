6e4434a6720aacce35702a486325686d7c0e1a04
wilder <anthonykwanchung@gmail.com>
2019-12-08 18:31:41 +0100

    slugify

--- src/Service/Slugify.php
+++ src/Service/Slugify.php
@@ -7,8 +7,9 @@ class Slugify
 {
     public function generate(string $input) : string
     {
-        $input = preg_replace('/-/', ' ', $input);
-
-        return preg_replace('/[^A-Za-z0-9\-]/', '', $input);;
+        $input = str_replace('-', ' ', $input);
+        $input = strtolower($input);
+        $input = preg_replace('/--+/', '-', $input);
+        return preg_replace('/[^A-Za-z0-9\-]/', '', $input);
     }
 }
