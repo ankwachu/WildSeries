859d0f285a5a15958d8ee1af658614c7d28558e0
wilder <anthonykwanchung@gmail.com>
2019-12-12 20:57:10 +0100

    install mailer and remove swiftmailer

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -9,6 +9,8 @@ use App\Service\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -28,8 +30,9 @@ class ProgramController extends AbstractController
 
     /**
      * @Route("/new", name="program_new", methods={"GET","POST"})
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
-    public function new(Request $request): Response
+    public function new(Request $request, MailerInterface $mailer): Response
     {
         $program = new Program();
         $form = $this->createForm(ProgramType::class, $program);
@@ -40,6 +43,15 @@ class ProgramController extends AbstractController
             $entityManager->persist($program);
             $slugify = new Slugify();
             $program->setSlug($slugify->generate($program->getTitle()));
+
+            $email = (new Email())
+                ->from('phptestwild@gmail.com')
+                ->to('phptestwild@gmail.com')
+                ->subject('Une nouvelle série vient d\'être publiée !')
+                ->html('<p>Une nouvelle série vient d\'être publiée sur Wild Séries !</p>');
+
+            $mailer->send($email);
+
             $entityManager->flush();
 
             return $this->redirectToRoute('program_index');
@@ -95,4 +107,6 @@ class ProgramController extends AbstractController
 
         return $this->redirectToRoute('program_index');
     }
+
+
 }
