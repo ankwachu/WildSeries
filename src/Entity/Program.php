e7392c2e23f75d60d09ee1679534b15c0297a46c
wilder <anthonykwanchung@gmail.com>
2019-12-20 18:24:31 +0100

    quete21

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -5,9 +5,15 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProgramRepository")
+ * @UniqueEntity(
+ *     fields={"title"},
+ *     message="Ce titre existe déjà"
+ * )
  */
 class Program
 {
@@ -20,11 +26,22 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Length(
+     *     max = 255,
+     *     maxMessage = "Le titre {{ value }} est trop long, il ne doit pas dépasser {{ limit }} caractères"
+     * )
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     * @Assert\Regex(
+     * pattern = "/plus belle la vie/",
+     * match = false,
+     * message = "On parle de vraie série ici"
+     * )
      */
     private $summary;
 
