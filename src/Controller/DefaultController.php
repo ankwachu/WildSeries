09df0479b9624bbc7663db7a07fab69a08901200
wilder <anthonykwanchung@gmail.com>
2019-12-20 14:29:31 +0100

    quete20

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,15 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="app_index")
      */
-    public function index() :Response
+    public function index(ProgramRepository $programRepository) :Response
     {
-        return $this->render('home.html.twig');
+        $programs = $programRepository->findBy(
+            [],
+            ['id' => 'DESC'],
+            3
+        );
+        return $this->render('home.html.twig', [
+            'programs' => $programs,
+        ]);
     }
 }
